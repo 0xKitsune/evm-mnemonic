3bf5b8079a59929663015cfdc0d0ba664bd8f85d
0xKitsune <0xKitsune@protonmail.com>
2022-08-05 02:06:19 -0400

    added parse_file

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -3,3 +3,13 @@ use pest::Parser;
 #[derive(Parser)]
 #[grammar = "evmm.pest"]
 pub struct EVMMParser;
+
+fn parse_file(unparsed_file: String) {
+    let file = EVMMParser::parse(Rule::file, &unparsed_file)
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next()
+        .unwrap(); // get and unwrap the `file` rule; never fails
+}
+
+#[test]
+fn test_parse_file() {}
