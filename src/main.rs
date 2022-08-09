61d4650847d40d0b6eafb6c8f7a312c1e7d845ff
0xKitsune <0xKitsune@protonmail.com>
2022-08-09 10:48:00 -0400

    added cli args with clap

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,10 @@ extern crate pest_derive;
 
 fn main() -> Result<(), EVMMError> {
     let matches = Command::new("evmm")
-        .about("A barebones framework to write hand tuned smart contracts")
+        .about("A barebones framework to write hand tuned smart contracts in pure opcodes.")
         .version("0.0.1")
         .subcommand_required(true)
         .arg_required_else_help(true)
-        .author("0xKitsune")
         //
         // compile subcommand
         .subcommand(
@@ -26,8 +25,7 @@ fn main() -> Result<(), EVMMError> {
                         .long("directory")
                         .short('d')
                         .help("Specify a directory to target for compilation. Ex: compile -d ./my_contracts")
-                        .conflicts_with("bytecode").
-                        conflicts_with("output")
+                        .conflicts_with("bytecode")
                         .action(ArgAction::Set)
                         .number_of_values(1),
                 )
@@ -36,16 +34,13 @@ fn main() -> Result<(), EVMMError> {
                         .long("bytecode")
                         .short('b')
                         .conflicts_with("directory")
-                        .help("view package information")
+                        .help("Prints the compiled bytecode for a specific contract into the terminal.")
                         .action(ArgAction::Set)
-                        .number_of_values(1),
                 ) .arg(
                     Arg::new("output")
                         .long("output")
                         .short('o')
-                        .conflicts_with("directory").
-                        conflicts_with("bytecode")
-                        .help("view package information")
+                        .help("Outputs the compiled bytecode for any contract that was compiled. If an output directory is not specified, the output will be written to `evmasm/` by default.")
                         .action(ArgAction::Set)
                         .number_of_values(1),
                 ),
@@ -54,8 +49,8 @@ fn main() -> Result<(), EVMMError> {
         // init subcommand
         .subcommand(
             Command::new("init")
+                .long_flag("init") 
                 .short_flag('i')
-                .long_flag("init")
                 .about("Creates a new Foundry project with all the moving parts to test EVMM contracts")
         )
         .get_matches();
