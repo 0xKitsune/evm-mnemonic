26549739e88d0a3ca57e9e2b15fcc0d8f432d6dd
0xKitsune <0xKitsune@protonmail.com>
2022-08-09 10:26:23 -0400

    Added clap for cli args

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,61 @@ mod compiler;
 mod evmm_error;
 mod parser;
 use crate::evmm_error::evmm_error::EVMMError;
-
+use clap::{Arg, ArgAction, Command};
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
 fn main() -> Result<(), EVMMError> {
-    let args: Vec<String> = env::args().skip(1).collect();
-
-    if args.len() == 0 {
-        //TODO: print flag help messages
-    } else {
-        if args.contains(&"compile".to_string()) {
-            //TODO: implement default compilation path, looks for the current folder ./src or something
-
-            //TODO: flag to check if there is a specified path
-
-            //TODO: flag to check if there is a specified contract to compile
-
-            //TODO: flag to output bytecode to a file
-
-            let unparsed_file = fs::read_to_string("example_contract.evmm")
-                .expect("Something went wrong reading the file");
-
-            let contract_bytecode =
-                parser::parse::evmm_parse("example_contract".to_owned(), &unparsed_file)?;
-
-            println!("{:?}", contract_bytecode);
-        }
-    }
+    let matches = Command::new("evmm")
+        .about("A barebones framework to write hand tuned smart contracts")
+        .version("0.0.1")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .author("0xKitsune")
+        //
+        // compile subcommand
+        .subcommand(
+            Command::new("compile")
+            .about("Compile .evmm contracts to bytecode. If a specific contract is provided, the compile command will only compile that contract. If no contract is provided, the compile command will compile everything in `./src/contracts/` by default. You can set the target directory to compile with the `-d` option. See `compile --help` for full usage.")
+                .arg(
+                    Arg::new("directory")
+                        .long("directory")
+                        .short('d')
+                        .help("Specify a directory to target for compilation. Ex: compile -d ./my_contracts")
+                        .conflicts_with("bytecode").
+                        conflicts_with("output")
+                        .action(ArgAction::Set)
+                        .number_of_values(1),
+                )
+                .arg(
+                    Arg::new("bytecode")
+                        .long("bytecode")
+                        .short('b')
+                        .conflicts_with("directory")
+                        .help("view package information")
+                        .action(ArgAction::Set)
+                        .number_of_values(1),
+                ) .arg(
+                    Arg::new("output")
+                        .long("output")
+                        .short('o')
+                        .conflicts_with("directory").
+                        conflicts_with("bytecode")
+                        .help("view package information")
+                        .action(ArgAction::Set)
+                        .number_of_values(1),
+                ),
+        )
+        //
+        // init subcommand
+        .subcommand(
+            Command::new("init")
+                .short_flag('i')
+                .long_flag("init")
+                .about("Creates a new Foundry project with all the moving parts to test EVMM contracts")
+        )
+        .get_matches();
 
     Ok(())
 }
