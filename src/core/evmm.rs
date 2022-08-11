964c19870fa02a5e60d8c013154e6646d4fb5915
0xKitsune <0xKitsune@protonmail.com>
2022-08-11 08:51:32 -0400

    removed unneeded return statements

--- src/core/evmm.rs
+++ src/core/evmm.rs
@@ -137,7 +137,6 @@ fn output_contracts(
     for evmasm_file in evmasm_files {
         //If an output directory is specified, write to a file
         if print_in_terminal == false {
-            println!("isfalse");
             if fs::metadata(output_directory).is_ok() {
                 let mut new_evmasm_file =
                     File::create(format!("{}/{}", output_directory, evmasm_file.file_name))
